ccc0c6f7059ee0786402f1ecc992eaa0ebe62716
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-01-08 17:37:19 +0530

    Add P1

--- day_3/54.spiral-matrix-python3.py
+++ day_3/54.spiral-matrix-python3.py
@@ -1,5 +1,12 @@
 # @leet start
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        
+        ans = []
+        while matrix:
+            ans.extend(matrix[0])
+            matrix = [
+                [ matrix[ro][col] for ro in range(1, len(matrix))] for col in range(len(matrix[0])-1,-1,-1)
+            ] # adds the first row, then transposes the remaining matrix
+        return ans
+
 # @leet end
