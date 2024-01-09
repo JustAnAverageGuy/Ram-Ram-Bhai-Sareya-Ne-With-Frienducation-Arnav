f4d5d69abd0819f40669f606152fb7d0409682a2
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-01-09 17:44:45 +0530

    Solve part 2

--- day_4/1423.maximum-points-you-can-obtain-from-cards-python3.py
+++ day_4/1423.maximum-points-you-can-obtain-from-cards-python3.py
@@ -2,4 +2,19 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         
+        n                  = len(cardPoints)
+        length_of_excluded = n-k
+        
+        total_sum          = sum(cardPoints)
+        curr               = sum(cardPoints[:length_of_excluded]) # could have been done earlier,
+                                                                  # but doesn't really matter
+        min_exclude        = curr
+
+        for i in range(0, k):
+            curr += cardPoints[i+n-k] - cardPoints[i] # in the i-th iteration
+                                                      # the first i+1 cards are considered
+            min_exclude = min(curr, min_exclude)
+
+        return (total_sum - min_exclude)
+
 # @leet end
--- day_4/53.maximum-subarray-python3.py
+++ day_4/53.maximum-subarray-python3.py
@@ -1,10 +1,12 @@
 # @leet start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # max_pre holds the maximum subarray sum ending at i-th index
         max_pre = [nums[0]]
         for i in nums[1:]:
             max_ending_here = i + max(max_pre[-1], 0) 
             max_pre.append(max_ending_here)
-        return max(max_pre)
+        return max(max_pre) # this could have been done 
+                            # inside of the loop, but this is more readable
 
 # @leet end
