94d771a52031bc8768614f2c730284e18817f415
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-01-10 18:23:47 +0530

    Solve part 2

---
+++ day_5/88.merge-sorted-array-python3.py
@@ -0,0 +1,10 @@
+# @leet start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # arrays can be atmost 200 elemnts long even when combined, so this is fine
+        for i in range(n): nums1[m+i] = nums2[i]
+        nums1.sort()
+# @leet end
