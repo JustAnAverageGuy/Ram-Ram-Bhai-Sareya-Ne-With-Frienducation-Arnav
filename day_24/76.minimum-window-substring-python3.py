297473751c7dba706cc5e8240b0a26b0b300f289
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-01-29 18:10:19 +0530

    Optimize part 1

--- day_24/76.minimum-window-substring-python3.py
+++ day_24/76.minimum-window-substring-python3.py
@@ -4,13 +4,22 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         t_cnt = defaultdict(int)
         for i in t: t_cnt[i] += 1
+        missing = len(t_cnt)
+        
         s_cnt = defaultdict(int)
+        
         l = 0
+        
         min_len = float('inf')
+        
         answer_indices = [0,-1]
+
         for r in range(len(s)):
             s_cnt[s[r]] += 1
-            if any(s_cnt[c] < t_cnt[c] for c in t_cnt): continue
+            
+            missing -= (t_cnt[s[r]] and t_cnt[s[r]] == s_cnt[s[r]])    
+            if missing: continue
+            
             while True:
                 x = s[l]
                 if s_cnt[x] > t_cnt[x]:
@@ -18,9 +27,11 @@ class Solution:
                     l += 1
                 else:
                     break
+
             if r-l+1 < min_len:
-                min_len = r - l + 1
+                min_len = r-l+1
                 answer_indices = [l,r]
+
         return s[answer_indices[0]:answer_indices[1]+1]
 
 
