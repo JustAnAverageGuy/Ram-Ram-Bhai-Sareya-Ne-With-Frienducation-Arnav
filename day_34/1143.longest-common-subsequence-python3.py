e81955ef15151a4109f528d50de630608d2f1658
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-02-09 21:53:42 +0530

    Solve LCS as well

--- day_34/1143.longest-common-subsequence-python3.py
+++ day_34/1143.longest-common-subsequence-python3.py
@@ -1,5 +1,12 @@
+from functools import cache
 # @leet start
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        @cache
+        def dp(i,j):
+            # return len of lcs in text1[:i] and text2[:j] 
+            if i*j == 0: return 0
+            return max( (1+dp(i-1, j-1)) if text1[i-1] == text2[j-1] else 0, dp(i,j-1), dp(i-1,j))
+        return dp(len(text1), len(text2))
         
 # @leet end
